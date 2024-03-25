219025a28a29055ba8090b43600ff5d23f505d1d
llii0046 <luxingg.li@gmail.com>
2024-03-25 11:08:59 +1100

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  ConfigurationKeyPaths,
+  getConfiguration,
+} from './config/configuration';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [getConfiguration],
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<ConfigurationKeyPaths>) => {
+        return {
+          autoLoadEntities: true,
+          type: configService.get<any>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+          password: configService.get<string>('database.password'),
+          database: configService.get<string>('database.database'),
+          synchronize: configService.get<boolean>('database.synchronize'),
+          logging: configService.get('database.logging'),
+        };
+      },
+    }),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
